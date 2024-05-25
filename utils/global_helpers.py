97d67a3e68cf160ed3b44f5b51f084b23055a154
rory-holmes <roryholmes2@gmail.com>
2024-05-25 18:32:39 +1200

    Graphing update

--- utils/global_helpers.py
+++ utils/global_helpers.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import yaml
 import pickle
-with open("params\model_params.yaml", "r") as f:
+with open("/csse/users/rho66/Desktop/Years/4/SENG402/SENG402/params/model_params.yaml", "r") as f:
     model_params = yaml.load(f, Loader=yaml.SafeLoader)
 
 def show_results(name):
-    with open(f"\results\{name}_train_history", 'wb') as file_pi:
+    with open(f"results\{name}_train_history(1)", 'rb') as file_pi:
         history = pickle.load(file_pi)    
-
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-    binary_cross_entropy= history.history['BinaryCrossentropy']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+    print(history)
+    acc = history['accuracy']
+    val_acc = history['val_accuracy']
+    precision= history['precision']
+    recall=history['recall']
+    loss = history['loss']
+    val_loss = history['val_loss']
 
     epochs_range = range(model_params['epochs'])
 
@@ -20,7 +21,8 @@ def show_results(name):
     plt.subplot(1, 2, 1)
     plt.plot(epochs_range, acc, label='Training Accuracy')
     plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.plot(epochs_range, binary_cross_entropy, label='BinaryCrossentropy')
+    plt.plot(epochs_range, precision, label='Precision')
+    plt.plot(epochs_range, recall, label="Recall")
     plt.legend(loc='lower right')
     plt.title('Training and Validation Accuracy')
 
@@ -29,11 +31,12 @@ def show_results(name):
     plt.plot(epochs_range, val_loss, label='Validation Loss')
     plt.legend(loc='upper right')
     plt.title('Training and Validation Loss')
-    plt.savefig(f"\results\{name}_history_graph.png")
+    plt.savefig(f"results\{name}_history_graph.png")
     plt.show()
 
 def save_history(history,  name):
-    path = f"\results\{name}_train_history"
+    path = f"results\{name}_train_history"
     with open(path, 'wb') as file_pi:
         pickle.dump(history.history, file_pi)
     
+#show_results("ResNet50")
